920914e25bf4bb2dc743fb102a5ff872e7045b2d
lsheon0927@naver.com <lsheon0927@naver.com>
2024-01-21 15:09:55 +0900

    fix: 포스트 미선택시 예외 처리 추가

--- src/main/java/com/example/blogalarm/controller/CommentController.java
+++ src/main/java/com/example/blogalarm/controller/CommentController.java
@@ -62,27 +62,38 @@ public class CommentController {
     @PostMapping("/comments/create")
     // TODO: session에서 로그인한 멤버 ID를 가져오기
 
-    public String createComment(@ModelAttribute CommentForm commentForm, Principal principal, HttpSession session ) {
+    public String createComment(@ModelAttribute CommentForm commentForm,  HttpSession session ) {
         Comment comment = new Comment();
         comment.setText(commentForm.getText());
         Member member = (Member) session.getAttribute("loggedInMember");
-
-
         System.out.println("Member : " +member);
 
        // Member member= memberRepository.findById(Long.valueOf(id)).orElseThrow(null);
         // TODO: Optional의 사용법과 쓰는 이유
-
         comment.setMember(member);
-
         // Comment와 Post를 연결
-        Post post = postService.getPostById(commentForm.getPostId());
+//        Post post = postService.getPostById(commentForm.getPostId());
+
+        Long postId = commentForm.getPostId(); // 폼에서 제공된 postId
+
+        if (postId == null || postService.getPostById(postId) == null) {
+            // 포스트가 유효하지 않은 경우, 오류 메시지와 함께 폼으로 다시 리다이렉트
+            session.setAttribute("errorMessage", "유효하지 않은 포스트 입니다. 다시 선택하세요.");
+            return "redirect:/comments/create";
+        }
+
+
+        Post post = postService.getPostById(postId);
+
+
         comment.setPost(post);
         commentService.saveComment(comment);
-
-
         return "redirect:/comments";
     }
+
+
+
+
 
     @GetMapping("/comments/edit/{id}")
     public String editCommentForm(@PathVariable Long id, Model model) {
