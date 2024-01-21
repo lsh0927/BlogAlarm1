140b3d5a5ca6555ac4f754fea5c36aad43a5007f
lsheon0927@naver.com <lsheon0927@naver.com>
2024-01-21 15:52:31 +0900

    fix: 댓글 감지를 통해 카카오 메세지 푸시 기능

--- src/main/java/com/example/blogalarm/service/CommentServiceImpl.java
+++ src/main/java/com/example/blogalarm/service/CommentServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.blogalarm.service;
 
 import com.example.blogalarm.domain.Comment;
+import com.example.blogalarm.domain.Member;
+import com.example.blogalarm.domain.Post;
 import com.example.blogalarm.repository.CommentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -9,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@Slf4j
 public class CommentServiceImpl implements CommentService {
 
     private final CommentRepository commentRepository;
@@ -39,10 +43,32 @@ public class CommentServiceImpl implements CommentService {
     }
 
     //댓글 저장
+//    @Override
+//    public void saveComment(Comment comment) {
+//        commentRepository.save(comment);
+//    }
     @Override
     public void saveComment(Comment comment) {
-        commentRepository.save(comment);
+        commentRepository.save(comment); // 댓글 저장
+
+        // 댓글이 달린 포스트의 작성자 정보를 가져옴
+        Post post = comment.getPost();
+        if (post != null) {
+            Member postAuthor = post.getMember();
+
+            if (postAuthor!=null){
+                log.info("Post Author Email: " + postAuthor.getEmail() + ", Nickname: " + postAuthor.getNickname());
+            // 카카오톡 메시지 전송 로직 호출 (예시)
+                 sendKakaoMessage(postAuthor);
+            }
+        }
     }
+
+    private void sendKakaoMessage(Member member) {
+        // 카카오톡 메시지 전송 로직 구현
+        // 여기서 member 객체의 정보(예: 이메일, 카카오톡 ID)와 엑세스 토큰을 사용
+    }
+
 
     //댓글 수정
     @Override
